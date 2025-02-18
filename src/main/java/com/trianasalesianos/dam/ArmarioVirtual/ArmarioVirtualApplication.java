82f9e7610fba0ece387ab11505fda0abd3322a9e
Patricia Alonso Ayllón <alonso.aypat22@salesianos.triana.edu>
2025-02-18 13:52:30 +0100

    proyecto bien creado

---
+++ src/main/java/com/trianasalesianos/dam/ArmarioVirtual/ArmarioVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.trianasalesianos.dam.ArmarioVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmarioVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmarioVirtualApplication.class, args);
+	}
+
+}
