53d8f6574f6ab65175969f56cddab10ab5d10ca1
Patty <alonso.aypat22@triana.salesianos.edu>
2025-06-16 11:46:00 +0200

    interfaz crear prenda

--- frontend/armarioVirtual/src/app/side-nav/side-nav.component.ts
+++ frontend/armarioVirtual/src/app/side-nav/side-nav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-side-nav',
-  standalone: true,
-  imports: [],
-  templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.css'
+selector: 'app-side-nav',
+standalone: true,
+imports: [ RouterModule ],
+templateUrl: './side-nav.component.html',
+styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent {
 
